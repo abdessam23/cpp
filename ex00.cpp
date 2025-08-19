044690a80ac7a6a8158ddd12358758c5b43f6d7e
abdo <dealkalr@gmail.com>
2025-08-19 17:41:25 +0100

    ok

---
+++ ex00.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cctype>
+
+class megaphone
+{
+public:
+    char* uppercase(char* str)
+    {
+        if (!str)
+            return NULL;
+        for (int i = 0; str[i] ; i++)
+        {
+            if (islower(str[i]))
+                str[i] -= 32; 
+        }
+        return (str);
+    }
+    void printmega(char* message)
+    {
+        std::cout << message ;
+    }
+};
+
+
+
+
+int main(int ac, char** argv)
+{
+    megaphone str;
+    if (ac < 2)
+        return 0;
+    for(int i = 1; i < ac; i++)
+    {
+        str.printmega(str.uppercase(argv[i]));
+    }
+    std::cout << "" << std::endl;
+}
