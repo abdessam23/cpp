9f41673dd6f632d4d59935296287e2e01b46ac88
abdo <dealkalr@gmail.com>
2025-08-19 18:53:16 +0100

    ok

--- ex00.cpp
+++ ex00.cpp
@@ -19,19 +19,23 @@ public:
     {
         std::cout << message ;
     }
+    void printerror()
+    {
+        std::cout << "* Usage : arg[0] arg[1] ... *" << std::endl;
+    }
 };
-
-
-
 
 int main(int ac, char** argv)
 {
     megaphone str;
     if (ac < 2)
+    {
+        str.printerror();
         return 0;
+    }
     for(int i = 1; i < ac; i++)
     {
         str.printmega(str.uppercase(argv[i]));
     }
-    std::cout << "" << std::endl;
+    std::cout << std::endl;
 }
