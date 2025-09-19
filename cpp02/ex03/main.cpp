cd64d46f7f87ecd38c123f4870a984cf3d1fc1cf
abdo <dealkalr@gmail.com>
2025-09-19 18:46:50 +0100

    ok

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,24 +6,31 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 15:24:25 by abdo              #+#    #+#             */
-/*   Updated: 2025/09/19 10:39:30 by abdo             ###   ########.fr       */
+/*   Updated: 2025/09/19 18:46:15 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "Fixed.hpp"
+#include "Point.hpp"
 
 
 int main() 
 {
-    Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-     std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max( a, b ) << std::endl;
+    Point a;
+    Point b(a);
+    Point c(1.2,3);
+    Point v(4.55,5.4545);
+    std::cout << a.getx() << std::endl;
+    std::cout << a.gety() << std::endl;
+
+    std::cout << b.getx() << std::endl;
+    std::cout << b.gety() << std::endl;
+    
+    std::cout << c.getx() << std::endl;
+    std::cout << c.gety() << std::endl;
+    if (bsp(a,b,c,v))
+        std::cout << "true" <<std::endl;
+    else 
+         std::cout << "false" <<std::endl;
     return 0;
 }
