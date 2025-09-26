5db0ea3e8d2f6a93418a7692f32b52ebbd4dfb3e
abdo <dealkalr@gmail.com>
2025-09-26 18:57:20 +0100

    ok

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 11:58:40 by abdo              #+#    #+#             */
-/*   Updated: 2025/09/26 15:41:20 by abdo             ###   ########.fr       */
+/*   Updated: 2025/09/26 16:05:04 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ ClapTrap::~ClapTrap()
 
 void ClapTrap::attack(const std::string& target)
 {
-    std::cout << "ClapTrap "<< name  << "attacks "<<  target << ", causing" << attack_damage << "points of damage!" << std::endl;
+    std::cout << "ClapTrap "<< name  << "attacks "<<  target << ", causing " << attack_damage << " points of damage!" << std::endl;
 }
 
 void ClapTrap::TakeDamage(unsigned int ammount)
@@ -32,8 +32,9 @@ void ClapTrap::TakeDamage(unsigned int ammount)
     if (hit_point > 0 && hit_point >= ammount)
     {
         hit_point -= ammount;
+        attack_damage++;
     }
-    std::cout << "ClapTrap takes damage!" << std::endl;
+    std::cout << "ClapTrap takes damage! "<< "hit_point: "<< hit_point << " damage: "<< attack_damage  << std::endl;
 }
 void ClapTrap::beRepaired(unsigned int ammount)
 {
@@ -42,5 +43,5 @@ void ClapTrap::beRepaired(unsigned int ammount)
         hit_point += ammount;
         energy_point--;
     }
-    std::cout << "ClapTrap repaired his hit point!" << std::endl;
+    std::cout << "ClapTrap repaired his hit point! " << "hit_point: "<< hit_point << " energie : "<< energy_point  << std::endl;
 }
