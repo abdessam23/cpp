6c4d0c82632098f985dc999e466787797ac10ecc
abdo <dealkalr@gmail.com>
2025-09-26 15:54:19 +0100

    imp main

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 11:58:40 by abdo              #+#    #+#             */
-/*   Updated: 2025/09/26 10:47:06 by abdo             ###   ########.fr       */
+/*   Updated: 2025/09/26 15:41:20 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,33 @@
 
 ClapTrap::ClapTrap(std::string name): name(name),hit_point(10),energy_point(10),attack_damage(0)
 {
+    std::cout << "Constractor called!" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
 {
+    std::cout << "destractor called!" << std::endl;
 }
 
 void ClapTrap::attack(const std::string& target)
 {
-    std::cout << "ClapTrap "<< name  << "attacks "<<  target<< ", causing" << attack_damage << "points of damage!" << std::endl;
+    std::cout << "ClapTrap "<< name  << "attacks "<<  target << ", causing" << attack_damage << "points of damage!" << std::endl;
 }
+
 void ClapTrap::TakeDamage(unsigned int ammount)
 {
-    
+    if (hit_point > 0 && hit_point >= ammount)
+    {
+        hit_point -= ammount;
+    }
+    std::cout << "ClapTrap takes damage!" << std::endl;
 }
 void ClapTrap::beRepaired(unsigned int ammount)
 {
-    
+    if (energy_point > 0 )
+    {
+        hit_point += ammount;
+        energy_point--;
+    }
+    std::cout << "ClapTrap repaired his hit point!" << std::endl;
 }
