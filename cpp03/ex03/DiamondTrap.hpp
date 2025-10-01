1e3deb3f9da4823973411055d8029e220729d355
abdo <dealkalr@gmail.com>
2025-10-01 15:56:34 +0100

    ok

--- cpp03/ex03/DiamondTrap.hpp
+++ cpp03/ex03/DiamondTrap.hpp
@@ -6,25 +6,32 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 15:02:50 by abdo              #+#    #+#             */
-/*   Updated: 2025/09/29 15:03:21 by abdo             ###   ########.fr       */
+/*   Updated: 2025/10/01 15:56:06 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
-class DiamondTrap
+class FragTrap: virtual ClapTrap{};
+class ScavTrap: virtual ClapTrap{}; 
+
+class DiamondTrap : public FragTrap,public ScavTrap
 {
 private:
-    /* data */
+   std::string name;
 public:
-    DiamondTrap(/* args */);
+    DiamondTrap(std::string name);
     ~DiamondTrap();
+    void whoAmI();
 };
 
-DiamondTrap::DiamondTrap(/* args */)
+DiamondTrap::DiamondTrap(std::string name): FragTrap(name)
 {
 }
-
+void DiamondTrap::whoAmI()
+{
+    std::cout << "The DiamondTrap " << name << 
+}
 DiamondTrap::~DiamondTrap()
 {
 }
