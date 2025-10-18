76d236224328949a5987c030a2200a19ee77469c
abdo <dealkalr@gmail.com>
2025-10-18 17:53:47 +0100

    ok

--- cpp11/ex06/Harl.cpp
+++ cpp11/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 15:03:13 by abdo              #+#    #+#             */
-/*   Updated: 2025/10/18 17:39:27 by abdo             ###   ########.fr       */
+/*   Updated: 2025/10/18 17:37:29 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,23 @@ void Harl::error()
     std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now."<< std::endl;
 }
-void Harl::complain( std::string level)
+void  Harl::complain( std::string level)
 {
     const std::string levels[]= {"DEBUG","INFO" ,"WARNING","ERROR"};
+    
     void (Harl::*f[])() = {&Harl::debug,&Harl::info, &Harl::warning, &Harl::error};
     for (int i = 0; i < 4;i++)
     {
         if (levels[i] == level)
         {
-            (this->*f[i])();
+            while (i < 4)
+            {
+               (this->*f[i])();
+               std::cout << std::endl;
+               i++;
+            }
             return;
         }
     }
-    std::cout << "nothing " << std::endl;
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
