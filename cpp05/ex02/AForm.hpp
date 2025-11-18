3583f53eb77e509b0e52030070f878a7949ca607
abdo <dealkalr@gmail.com>
2025-11-18 12:29:29 +0100

    ok

---
+++ cpp05/ex02/AForm.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 13:18:11 by abdo              #+#    #+#             */
+/*   Updated: 2025/11/17 15:41:39 by abdo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once 
+
+#include "Bureaucrat.hpp"
+#include <iostream>
+class Bureaucrat;
+
+class AForm
+{
+    private:
+        const std::string name;
+        bool n;
+        const int grade_to_s;
+        const int grade_to_x;
+        AForm();
+    public:
+        AForm(const std::string name,bool n,const int grade_to_s,const int grade_to_x);
+        AForm(const AForm& other);
+        AForm& operator=(const AForm& other);
+        ~AForm();
+        void beSigned(Bureaucrat& obj);
+        class GradeTooHighException: public std::exception
+        {
+            const char* what() const throw();
+        };
+        class GradeTooLowException: public std::exception
+        {
+            const char* what() const throw();
+        };
+        virtual void execute(Bureaucrat const & executor) const  = 0 ;
+        const std::string getName() const;
+        int get_Gts() const;
+        int get_Gtx() const;
+        bool getSign();
+        
+};
+std::ostream& operator<<(std::ostream& out, AForm& obj);
---
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 12:28:40 by abdo              #+#    #+#             */
+/*   Updated: 2025/11/18 12:29:15 by abdo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#pragma once 
+
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm
+{
+private:
+    /* data */
+public:
+    PresidentialPardonForm(/* args */);
+    ~PresidentialPardonForm();
+};
+
+PresidentialPardonForm::PresidentialPardonForm(/* args */)
+{
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
