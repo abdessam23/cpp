87bb6e7b70bfd8f62b3c4c809de2aa0a05497d46
abdo <dealkalr@gmail.com>
2025-11-20 13:26:27 +0100

    ok

---
+++ cpp05/ex03/Intern.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 13:21:13 by abdo              #+#    #+#             */
+/*   Updated: 2025/11/20 13:25:06 by abdo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+
+class Intern
+{
+    private:
+    public:
+        Intern();
+        ~Intern();
+        AForm* makeForm(std::string s1,std::string s2);
+};
+
+Intern::Intern()
+{
+}
+AForm* makeForm(std::string form,std::string target)
+{
+    
+}
+Intern::~Intern()
+{
+}
