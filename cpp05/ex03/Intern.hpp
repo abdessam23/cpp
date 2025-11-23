75fc2a195d831ef8e04cb80e21361d2880b6af59
abdo <dealkalr@gmail.com>
2025-11-23 12:56:24 +0100

    ok

--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -6,11 +6,14 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 13:21:13 by abdo              #+#    #+#             */
-/*   Updated: 2025/11/23 12:37:55 by abdo             ###   ########.fr       */
+/*   Updated: 2025/11/23 12:55:55 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 class Intern
 {
@@ -21,6 +24,11 @@ class Intern
         Intern& operator=(const Intern& other);
         ~Intern();
         AForm* makeForm(std::string s1,std::string s2);
+        class Internexcept: public std::exception
+        {
+          const char* what() const throw();  
+        };
+        
 };
 
 Intern::Intern()
@@ -30,15 +38,27 @@ AForm* makeForm(std::string form,std::string target)
 {
        try
     {
-        AForm* 
-        if (form )
-        std::cout << "Intern creates " << 
+       AForm* aform;
+        if (form == "Shrubbery")
+           aform = new ShrubberyCreationForm(target);
+        else if (form == "Robotomy")
+           aform = new RobotomyRequestForm(target);
+        else if (form == "Presidential")
+           aform = new PresidentialPardonForm(target);
+        else
+            throw(Intern::Internexcept());
+        std::cout << "Intern creates " << aform->getName() << std::endl;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
     
+}
+
+const char* Intern::Internexcept::what() const throw()
+{
+    return "There no form with this name. ";
 }
 Intern::~Intern()
 {
