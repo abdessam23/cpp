563a969a07203842a521416832ccc0d43bb39f3b
abdo <dealkalr@gmail.com>
2025-11-26 13:50:28 +0100

    ok

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 11:23:09 by abdo              #+#    #+#             */
-/*   Updated: 2025/11/26 13:16:39 by abdo             ###   ########.fr       */
+/*   Updated: 2025/11/26 13:49:50 by abdo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ void ScalarConverter::convert(std::string str)
             float n = atof(str.c_str());
             if (std::isnan(atof(str.c_str())) || std::isinf(atof(str.c_str())))
             {
-                std::cout << "float: "<< n<<"f"<<std::endl;
+                std::cout << "float: "<< n <<"f"<<std::endl;
             }
             else
                 std::cout << "float: "<< n << ".0f"<<std::endl;  
