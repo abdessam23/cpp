51aa9be42553bf6ff0b9a192bafdaf01d71d1b21
abdo <dealkalr@gmail.com>
2025-12-09 10:11:13 +0100

    just test

---
+++ cpp05/test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+void fun1()
+{
+    std::cout << "fun1 start "<< std::endl;
+    throw 10;
+    std::cout << " fun1 end "<< std::endl;
+}
+
+void fun2()
+{
+    std::cout << "fun2 start "<< std::endl;
+    fun1();
+    std::cout << "fun2 end "<< std::endl;
+}
+
+void fun3()
+{
+    std::cout << "fun3 start"<< std::endl;
+    fun2();
+    std::cout << "fun3 end"<<std::endl;
+}
+int main()
+{
+    try
+    {
+        fun3();   
+    }
+    catch( int  e)
+    {
+        std::cerr <<"throw int : "<< e << '\n';
+    }
+    std::cout << "the program end"<< std::endl;
+
+}
