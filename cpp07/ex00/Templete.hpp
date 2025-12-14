fd273677147dac630ee83e35f46d757dd17e11d5
abdessam23 <dealkalr@gmail.com>
2025-12-14 12:52:58 +0100

    ex00 done

--- cpp07/ex00/Templete.hpp
+++ cpp07/ex00/Templete.hpp
@@ -12,7 +12,7 @@
 
 #pragma once 
 
-template <typename T>void swap(T a, T b)
+template <typename T> void swap(T& a, T& b)
 {
     T tmp;
     tmp = a;
--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -18,5 +18,12 @@ int main()
    int a = 4;
    int b = 6;
    swap(a,b);
+   std::cout << "Swap"<< std::endl;
    std::cout << "a : " <<a << std::endl << "b : " <<b <<std::endl; 
+   
+   std::cout << "Min" << std::endl;
+   std::cout << min(a,b)<<std::endl; 
+  
+   std::cout << "Max"<< std::endl;
+   std::cout <<  max(a,b) <<std::endl; 
 }
