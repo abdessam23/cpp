c44af2ffb7a67a2fff9dc50fdf20e6b1eeefd113
userhm <dealkalr@gmail.com>
2025-12-23 11:16:10 +0100

    ok

--- cpp07/ex01/Iter.hpp
+++ cpp07/ex01/Iter.hpp
@@ -6,11 +6,47 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 12:33:47 by abhimi            #+#    #+#             */
-/*   Updated: 2025/12/22 13:22:59 by abhimi           ###   ########.fr       */
+/*   Updated: 2025/12/23 11:15:15 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template<typename T1,typename T2,typename T3> void Iter(const T1 ar[],const T2 len,const T3 size)
+template<typename T,int N> 
+class Array
 {
-    return;
+private:
+    T ar[N];
+public:
+    Array(const T ar[N]);
+    Array();
+    Array(const Array& other);
+    Array operator=(const Array& other);
+    ~Array();
+};
+
+
+template<typename T,int N> 
+Array(const T ar[N])
+{
+    
 }
+template<typename T,int N> 
+Array::Array()
+{
+    
+}
+template<typename T,int N> 
+Array::Array(const Array& other)
+{
+    
+}
+template<typename T,int N>
+Array::Array operator=(const Array& other)
+{
+
+    
+}
+Array::~Array()
+{
+    
+}
+
