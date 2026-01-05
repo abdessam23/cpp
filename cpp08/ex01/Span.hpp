baeaacb46bd6bed87afcc9a31cc8de8135e13d2f
userhm <dealkalr@gmail.com>
2026-01-05 11:53:24 +0100

    start ex01

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 10:56:21 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/05 11:18:04 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/05 11:51:51 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,8 @@ class Span
         Span(unsigned int n);
         Span(const Span& other);
         Span& operator=(const Span& other);
-        ~Span();     
+        ~Span();
+        void addNumber();
+        void shortestSpan();
+        void longestSpan();
 };
