57e51804384bcbdb85b7e4ad5e8be510e3140232
userhm <dealkalr@gmail.com>
2026-01-05 12:33:45 +0100

    70%

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 11:07:30 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/05 11:24:48 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/05 12:31:47 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,43 @@ Span& Span::operator=(const Span& other)
     return *this;
 }
 Span::~Span(){}
+
+void Span::addNumber(const int n)
+{
+    try{
+        if (s.size() <= N)
+            s.insert(n);
+        else
+            throw std::out_of_range("span is full");
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() <<std::endl;
+    }
+}
+int Span::shortestSpan()
+{
+    try{
+        if (s.size() == 0 || s.size() == 1)
+            return(s[1] - s[0]);
+        else
+            throw std::out_of_range("span is full");
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() <<std::endl;
+    }
+}
+int Span::longestSpan()
+{
+    try{
+        if (s.size() == 0 || s.size() == 1)
+            return(s[s.size() - 1] - s[0]);
+        else
+            throw std::out_of_range("span is full");
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() <<std::endl;
+    }
+}
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,23 +6,26 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 10:56:21 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/05 11:51:51 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/05 12:31:31 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <set>
+#include <exception>
 
 class Span
 {
     private:
         unsigned int N;
+        std::set<int> s;
         Span();
     public:
         Span(unsigned int n);
         Span(const Span& other);
         Span& operator=(const Span& other);
         ~Span();
-        void addNumber();
-        void shortestSpan();
-        void longestSpan();
+        void addNumber(const int n);
+        int shortestSpan();
+        int longestSpan();
 };
