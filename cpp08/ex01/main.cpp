bbb021fd97b1300eec1093b474369f97196bd027
userhm <dealkalr@gmail.com>
2026-01-06 11:29:52 +0100

    done

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 11:19:12 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/06 11:13:24 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/06 11:28:11 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 
 int main()
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    // sp.addNumber(14);
-    // sp.addNumber(15);
-    // sp.addNumber(170);
-    // sp.addNumber(1723);
+    Span sp = Span(20000);
+    // sp.addNumber(6);
+    // sp.addNumber(3);
+    // sp.addNumber(17);
+    // sp.addNumber(9);
+    // sp.addNumber(11);
+    int j = 4 ;
+    for (int i = 0 ;i <= 10000;i++)
+    {
+        j++;
+        sp.addNumber(j);
+    }
     
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
