9aecc472902e3c42eed9105f7fc4dc10bee57b1e
userhm <dealkalr@gmail.com>
2026-01-10 12:31:46 +0100

    fix  some test in ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -15,65 +15,65 @@
 template<typename T> 
 class Array
 {
-private:
-    int N;
-    T* data;
-    Array():N(10)
-    {
-    }
-public:
-      
-    Array(const int size):N(size)
-    {
-     data = new T[size];
-     for(int i = 0; i < N;i++)
-     {
-        data[i] = 0;
-     }
-    }
-    Array(const Array& other):N(other.N),data(new T[other.N])
-    {
-    if (this != &other)
-    {
-        for (int i = 0; i < N ;i++)
+    private:
+        unsigned int N;
+        T* data;
+    public:
+         Array()
         {
-            data[i] =  other.data[i];
+            data = new T[0];
         }
-    }
-    }
-    Array& operator=(const Array& other)
-    {
+        Array(const int size):N(size)
+        {
+         data = new T[size];
+         for(size_t i = 0; i < N;i++)
+         {
+            data[i] = 0;
+         }
+        }
+        Array(const Array& other):N(other.N),data(new T[other.N])
+        {
         if (this != &other)
         {
-             N = other.N;
-             delete data;
-             data = new  T[other.N];
-            for (int i = 0; i < N ;i++)
+            for (size_t i = 0; i < N ;i++)
             {
                 data[i] =  other.data[i];
             }
         }
-        return *this;
-    }
-    
-    T& operator[](int index) const
-    {
-        if (index  < 0 || index >= N )
-            throw std::out_of_range("index out of range");
-        return data[index];
-    }
-    bool operator!=(Array& arr) const
-    {
-        for (int i = 0;i < N;i++)
-        {
-            if (data[i] != arr.data[i])
-                return true;
         }
-    }
-    ~Array()
-    {
-        delete[] data;
-    }
+        Array& operator=(const Array& other)
+        {
+            if (this != &other)
+            {
+                 N = other.N;
+                 delete data;
+                 data = new  T[other.N];
+                for (size_t i = 0; i < N ;i++)
+                {
+                    data[i] =  other.data[i];
+                }
+            }
+            return *this;
+        }
+
+        T& operator[](unsigned int index) const
+        {
+            if (index >= N )
+                throw std::out_of_range("index out of range");
+            return data[index];
+        }
+        bool operator!=(Array& arr) const
+        {
+            for (size_t i = 0;i < N;i++)
+            {
+                if (data[i] != arr.data[i])
+                    return true;
+            }
+        }
+        ~Array()
+        {
+            delete[] data;
+        }
 };
 
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 15:57:29 by abhimi            #+#    #+#             */
-/*   Updated: 2025/12/25 11:05:49 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/10 12:30:08 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 #include <iostream>
 #include "Array.hpp"
 #include <cstdlib>
-
-#define MAX_VAL 10
+#define MAX_VAL 720
 
 int main(int, char**)
 {
+    // Array<int> a;
+    // a = Array<int>(5);
+    // for (size_t  i =0; i < 5;i++)
+    // {
+    //     a[i] = i + 4;
+    // }
+    //  for (size_t  i = 0; i < 5;i++)
+    // {
+    //    std::cout <<  a[i] << std::endl;
+    // }
     Array<int> numbers(MAX_VAL);
     numbers[0] = 7;
     std::cout << numbers[0] << "\n";
