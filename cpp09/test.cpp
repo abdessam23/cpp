1a06f2402be9a2479bbc0e14a61f5609f3aad78a
userhm <dealkalr@gmail.com>
2026-01-12 12:49:24 +0100

    test

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -1,24 +1,5 @@
 #include <iostream>
 //........test insertion ..............
-// int  main()
-// {
-//     int arr[] = {4,5,8,7,9,1,0};
-//     for (int i =1;i < 7;i++)
-//     {
-//         int k = arr[i];
-//         int j = i -1;
-//        while (j >= 0 && arr[j] > k)
-//         {
-//            arr[j + 1] = arr[j];
-//            j = j-1;
-//         }
-//         arr[j + 1] = k;
-//     }
-//     for(int i =0;i < 7;i++)
-//     {
-//         std::cout << arr[i] << std::endl;
-//     }
-// }
 
 
 int main()
