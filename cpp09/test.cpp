de1a608a047dffa10083000f1bfa4a12c5d220b1
userhm <dealkalr@gmail.com>
2026-01-12 12:40:38 +0100

    test

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 //........test insertion ..............
-int  main()
+// int  main()
+// {
+//     int arr[] = {4,5,8,7,9,1,0};
+//     for (int i =1;i < 7;i++)
+//     {
+//         int k = arr[i];
+//         int j = i -1;
+//        while (j >= 0 && arr[j] > k)
+//         {
+//            arr[j + 1] = arr[j];
+//            j = j-1;
+//         }
+//         arr[j + 1] = k;
+//     }
+//     for(int i =0;i < 7;i++)
+//     {
+//         std::cout << arr[i] << std::endl;
+//     }
+// }
+
+
+int main()
 {
-    
-    int arr[] = {4,5,8,7,9,1,0};
-    for (int i =1;i < 7;i++)
+    int a[]= {8,4,2,6,0,99,43,3,71};
+    for (int i = 1; i < 9;i++)
     {
-        int k = arr[i];
-        int j = i -1;
-       while (j >= 0 && arr[j] >= k)
+        int j = i - 1;
+        int key = a[i];
+        while (j >= 0 && a[j] > key)
         {
-           arr[j + 1] = arr[j];
-           j = j-1;
+            a[j + 1] = a[j];
+            j--;
         }
-        arr[j + 1] = k;
+        a[j + 1] = key;
     }
-    for(int i =0;i < 7;i++)
+    for (int i = 0;i < 9;i++)
     {
-        std::cout << arr[i] << std::endl;
+        std::cout << a[i] <<std::endl;
     }
 }
