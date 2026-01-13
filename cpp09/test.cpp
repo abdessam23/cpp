50a866bc017500461a6aa75d6d92a905d5a72c2b
userhm <dealkalr@gmail.com>
2026-01-13 10:48:09 +0100

    test

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -4,7 +4,7 @@ int bsearch(int *a, int target)
 {
      int mid,left ,right;
     left = 0;
-    right = 8;
+    right = 1;
     while (left <= right)
     {
         mid = left + (right - left)/2;
@@ -13,12 +13,12 @@ int bsearch(int *a, int target)
         else if (a[mid] > target)
             right = mid;
         else
-            left = mid;
+            left = mid + 1 ;
     }
     return -1;
 }
 int main()
 {
-    int a[]= {0,2};
-    std::cout<< bsearch(a,43) << std::endl;
+    int a[]= {0,2,6,7};
+    std::cout<< bsearch(a,3) << std::endl;
 }
