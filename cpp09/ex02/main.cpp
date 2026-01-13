b8dbb19271c77d3776f501294e34c5ab9efae9a4
userhm <dealkalr@gmail.com>
2026-01-13 14:45:59 +0100

    parse input

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,12 +6,16 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 10:09:40 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/13 12:58:29 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/13 14:44:08 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <climits>
+
 int Is_valid(char* str)
 {
     int i = 0;
@@ -36,20 +40,21 @@ int check_arg(char **arg)
     return 1;
 }
 
-void fill_string(char** arg,std::string &str)
+void fill_string(char** arg,std::vector<std::string> &str)
 {
     int i = 1;
     std::string s;
      std::string tmp;
     while (arg[i])
     {
-       s += arg[i]; 
+       s += arg[i] ; 
+       s += " ";
        i++;
     }
     std::stringstream ss(s);
-    while(std::getline(ss,tmp,' '))
+    while(ss >>tmp)
     {
-        str += tmp;
+        str.push_back(tmp);
     }
 }
    
@@ -66,11 +71,24 @@ int main(int ac, char** arg)
         std::cerr<< "Invalid input"<< std::endl;
         return 1;
     }
-    std::string  str;
+    std::vector<std::string>  str;
+     std::vector<int>  arr;
     fill_string(arg,str);
-    for(int i = 0;i < str.length();i++)
+    
+    for(int i = 0;i < str.size();i++)
     {
-        std::cout << str[i] <<std::endl;
+        double n = std::strtod(str[i].c_str(),NULL);
+        if (n < INT_MIN || n > INT_MAX )
+        {
+            std::cerr << "Error : overflow" << std::endl;
+            return 1;
+        }
+        else
+            arr.push_back(n);
+    }
+    for(int i = 0;i < arr.size();i++)
+    {
+        std::cout << arr[i] <<std::endl;
     }
     
     return 0;
