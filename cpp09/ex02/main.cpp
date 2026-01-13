5458bdc022ed734fa9f208ef841bbf05afe3b016
userhm <dealkalr@gmail.com>
2026-01-13 12:35:58 +0100

    start with last ex

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,14 +6,49 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 10:09:40 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/12 10:11:11 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/13 12:35:31 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int Is_valid(char* str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        if (!isdigit(str[i]) && str[i] != ' ')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int check_arg(char **arg)
+{
+    int i =1;
+    while (arg[i])
+    {
+       if (!Is_valid(arg[i]))
+        return 0;
+       i++;
+    }
+    return 1;
+}
 int main(int ac, char** arg)
 {
-    if(ac == 1)
+    if(ac < 2)
     {
-        return 0;
+        std::cerr<< "Error please enter one integers or more "<< std::endl;
+        return 1;
     }
+    if (!check_arg(arg))
+    {
+        std::cerr<< "Invalid input"<< std::endl;
+        return 1;
+    }
+    
+    std::cout << "all fine" <<std::endl;
+    return 0;
+    
 }
