54f87389a4f54e637d10588161de4c26622463e7
userhm <dealkalr@gmail.com>
2026-01-13 11:40:57 +0100

    test

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 #include <vector>
 //........test insertion ..............
-int bsearch(std::vector<int>& a, int target)
-{
-    int left,mid,right;
-    left = 0;
-    right = a.size()-1;
-    while (left <= right)
-    {
-        mid = left +(right - left)/2;
-        if (a[mid] == target)
-            return mid;
-        else if (a[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return -1;
-}
+// int bsearch(std::vector<int>& a, int target)
+// {
+//     int left,mid,right;
+//     left = 0;
+//     right = a.size()-1;
+//     while (left <= right)
+//     {
+//         mid = left +(right - left)/2;
+//         if (a[mid] == target)
+//             return mid;
+//         else if (a[mid] < target)
+//             left = mid + 1;
+//         else
+//             right = mid - 1;
+//     }
+//     return -1;
+// }
 int main()
 {
-   std::vector<int> a= {2,7,9,12,14};
-    std::cout<< bsearch(a,7) << std::endl;
+   std::vector<int> a= {2,7,1,6,18,34,61,28,9,12,14};
+    for (int i = 1;i < a.size();i++)
+    {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0&& a[j] > key)
+        {
+            a[j + 1] =  a[j];
+            j--;
+        }
+        a[j+1] = key;
+    }
+    
+    for (int i = 1;i < a.size();i++)
+    {
+        std::cout<<a[i] << std::endl;
+    }
 }
+
+
