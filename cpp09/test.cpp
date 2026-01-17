90300811b9ec2aee4dc5d10353eeed64f6ab2247
userhm <dealkalr@gmail.com>
2026-01-17 13:30:56 +0100

    ok

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-// int bsearch(std::vector<int>& a, int target)
-// {
-//     int left,mid,right;
-//     left = 0;
-//     right = a.size()-1;
-//     while (left <= right)
-//     {
-//         mid = left +(right - left)/2;
-//         if (a[mid] == target)
-//             return mid;
-//         else if (a[mid] < target)
-//             left = mid + 1;
-//         else
-//             right = mid - 1;
-//     }
-//     return -1;
-// }
-
-
-int binarvysearch(std::vector<int>& a, int target)
+int binarysearch(std::vector<int>& a, int target)
 {
     int left = 0;
     int right = a.size();
