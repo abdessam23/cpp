5aa9e22bebc8a2af7695b626ced868d3a003aa7d
userhm <dealkalr@gmail.com>
2026-01-18 15:48:48 +0100

    test

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 // int binarysearch(std::vector<int>& a, int target)
 // {
 //     int left = 0;
@@ -19,6 +20,15 @@
 //     return -1;
 // }
 
+void swap(int& a,int& b)
+{
+    if (a > b)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+}
 
 int bnshearch(std::vector<int>& v, int target)
 {
@@ -32,24 +42,43 @@ int bnshearch(std::vector<int>& v, int target)
             return mid;
        else if (v[mid] < target)
             left = mid + 1;
-        else
+       else
             right = mid - 1;
     }
     return -1;
 }
+
 int main()
 {
    std::vector<int> a= {2,7,1,6,18,34,61,28,9,12,14};
    int h = a.size()/2;
-   std::vector<int> b;
-   for(int i = 0;i< h;i++)
+   for (int i = 0; i < a.size();i++)
    {
-        
+        if ((i  + 1 < a.size()) && (a[i] > a[i + 1]))
+        {
+            swap(a[i],a[i + 1]);
+            
+        }
+        i++;
+   }
+   std::vector<int> b;
+    for (int i = 0; i < h;i++)
+   {
+        b[i] = a[i];
+        // a.erase(a.begin() + i);
    }
 
-   std::sort(a.begin(),a.end());
-  
-std::cout<<bnshearch(a,6) << std::endl;
+    for (int i = 0; i < a.size();i++)
+   {
+        std::cout << a[i] << " ";
+   }
+   
+    for (int i = 0; i <b.size();i++)
+   {
+        std::cout << b[i] << " ";
+   }
+//    std::sort(a.begin(),a.end());
+//    std::cout<<bnshearch(a,6) << std::endl;
 
 }
 
