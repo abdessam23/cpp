a2193c8bb9fe20d88ac15c561da99622b7cd971f
userhm <dealkalr@gmail.com>
2026-01-19 16:08:25 +0100

    test

--- cpp09/test.cpp
+++ cpp09/test.cpp
@@ -2,170 +2,65 @@
 #include <vector>
 #include <algorithm>
 
-// int binarysearch(std::vector<int>& a, int target)
-// {
-//     int left = 0;
-//     int right = a.size();
-//     int mid;
-//     while (left<= right)
-//     {
-//         mid = left + (right - left)/2;
-//         if (a[mid] == target)
-//             return mid;
-//         else if (a[mid] < target)
-//             left = mid + 1;
-//         else 
-//             right = mid - 1;
-//     }
-//     return -1;
-// }
 
-// void swap(int& a,int& b)
-// {
-//     if (a > b)
-//     {
-//         int tmp = a;
-//         a = b;
-//         b = tmp;
-//     }
-// }
-
-// int bnshearch(std::vector<int>& v, int target)
-// {
-//     int left = 0;
-//     int right = v.size()-1;
-//     int mid;
-//     while (left<=right)
-//     {
-//        mid = left + (right - left)/2;
-//        if (v[mid] == target)
-//             return mid;
-//        else if (v[mid] < target)
-//             left = mid + 1;
-//        else
-//             right = mid - 1;
-//     }
-//     return -1;
-// }
-
-// int main()
-// {
-//    std::vector<int> a= {2,7,1,6,18,34,61,28,9,12,14};
-//    int h = a.size()/2;
-//    for (int i = 0; i < a.size();i++)
-//    {
-//         if ((i  + 1 < a.size()) && (a[i] > a[i + 1]))
-//         {
-//             swap(a[i],a[i + 1]);
-            
-//         }
-//         i++;
-//    }
-
-//    std::cout <<" a: ";
-//     for (int i = 0; i < a.size();i++)
-//    {
-//         if ((i  + 1 < a.size()))
-//         {
-//              std::cout << "{"<<a[i] << ","<< a[i+1] << "}" << " ";
-//             i++;
-//         }
-       
-//    }
-
-//    std::vector<int> b(a.size()/2 - 1);
-//     int j = 0;
-//      for (int i = 0; i < a.size();i++)
-//    {
-//         if (i + 1 < a.size())
-//         {
-//           p.first = a[i];
-//           p.second = a[i + 1];
-//             j++;
-//           if (i != 0)
-//                a.erase(a.begin() + i);
-//         }
-//    }
-
-//     std::cout <<"\na: ";
-//     for (int i = 0; i < a.size();i++)
-//    {
-//         std::cout << a[i] << " ";
-//    }
-
-//    std::cout <<"\nb: ";
-//    for (int i = 0; i <b.size();i++)
-//    {
-//         std::cout << b[i] << " ";
-//    }
-// //    std::sort(a.begin(),a.end());
-// //    std::cout<<bnshearch(a,6) << std::endl;
-
-// }
-void merge(std::vector<int>& P,int l,int m,int r)
+void merge(std::vector<int>& b,int l,int mid, int right)
 {
-     
-     int i = 0;
-     int n1 = m - l + 1;
-     int n2 = r - m;
+     int n1 = mid - l +1;
+     int n2 = right - mid;
      std::vector<int> c1(n1),c2(n2);
-     std::cout << "\nc1: ";
-     for(int i = 0;i<n1;i++)
+     //  std::cout <<  "\n c1:  ";
+     for (int i = 0; i < n1;i++)
      {
-          c1[i] = P[l + i];
-          std::cout << c1[i] << " ";
+          c1[i] = b[l + i];
+          // std::cout << c1[i] << " ";
      }
-     std::cout << "\nc2: ";
-     for(int i = 0;i< n2;i++)
+     //  std::cout <<  "\n c2:  ";
+     for (int i = 0; i < n2;i++)
      {
-          c2[i] = P[m + i + 1];
-          std::cout << c2[i] << " ";
+          c2[i] = b[mid + 1 + i];
+          // std::cout << c2[i] << " ";
      }
-       std::cout << "\n";
-     i = 0;
+     int i = 0;
      int j = 0;
      int k = l;
-     while (i <  n1 && j < n2)
+     while (i < n1 &&  j < n2)
      {
-          if (c1[i] <= c2[j])
-          {
-               P[k] = c1[i];
+         if (c1[i] <= c2[j])
+         {
+               b[k] = c1[i];
                i++;
-          }
-          else
-          {
-               P[k] = c2[j];
+         }
+         else
+         {
+               b[k] = c2[j];
                j++;
-          }
-          k++;
+         }
+         k++;
      }
 
      while (i < n1)
      {
-          P[k] = c1[i];
-          i++;
-          k++;
+        b[k] = c1[i];
+        i++;
+        k++;
      }
-
-     while (j < n2)
+      while (j < n2)
      {
-          P[k] = c2[j];
-          j++;
-          k++;
+        b[k] = c2[j];
+        j++;
+        k++;
      }
-     
 }
 
-void mergsort(std::vector<int>& t,int left,int right)
+void mergsort(std::vector<int>& p,int left,int right)
 {
      if(left >= right)
           return;
-     int mid = left+ (right - left)/2;
-     mergsort(t,left,mid);
-     mergsort(t,mid+1,right);
-     merge(t,left,mid,right);
+     int mid = left + (right - left)/2;
+     mergsort(p,left,mid);
+     mergsort(p,mid + 1,right);
+     merge(p,left,mid,right);
 }
-
 int main()
 {
    std::vector<int> a= {2,7,1,6,18,34,61,28,9,12,14};
