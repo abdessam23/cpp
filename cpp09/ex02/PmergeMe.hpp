4dc9a9a2b5f01b82548a8ed5fea2deeff190a989
userhm <dealkalr@gmail.com>
2026-01-20 11:46:39 +0100

    start

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,24 +6,42 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 10:03:46 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/20 08:52:57 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/01/20 11:44:52 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <utility>
+#include <vector>
 
 class PmergeMe
 {
 private:
+    std::vector<int> arr;
+    int n;
 public:
-    PmergeMe(/* args */);
+    PmergeMe();
+    PmergeMe(unsigned int n);
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
     ~PmergeMe();
+    void intopair(std::vector<int>& arr);
+    
+    
 };
 
-PmergeMe::PmergeMe(/* args */)
+PmergeMe::PmergeMe()
 {
 }
 
 PmergeMe::~PmergeMe()
 {
+}
+
+void PmergeMe::intopair(std::vector<int>& arr)
+{
+    for (int i = 0; i < n;i++)
+    {
+        
+    }
 }
