992880fe5b9d23a7e05d119ededcc35c0f63f5c0
userhm <dealkalr@gmail.com>
2026-01-25 21:38:34 +0100

    test

--- cpp07/test.cpp
+++ cpp07/test.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
-template<typename T>
- void fun(const T& p){
-    std::cout << p<< std::endl; 
- }
-
+#include "cc.hpp"
+// template<typename T>
+//  void fun(const T& p){
+//     std::cout << p<< std::endl; 
+//  }
+template<int> void fun(int a);
  int main()
  {
     // int a = 8;
