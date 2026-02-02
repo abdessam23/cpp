d5313f5191ec293e3a4028c4ca1633dd1c812c40
userhm <dealkalr@gmail.com>
2026-02-02 08:53:20 +0100

    ok

--- cpp07/ex00/Templete.hpp
+++ cpp07/ex00/Templete.hpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 14:14:42 by abdo              #+#    #+#             */
-/*   Updated: 2026/01/14 11:28:34 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/02 08:52:01 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ template <typename T> void swap(T& a, T& b)
     b = tmp;
 }
 
-template<typename T> T min(T a,T b)
+template<typename T> T min(const T& a,const T& b)
 {
     return (a < b) ? a : b;
 }
 
-template <typename T> T max(T a,T b)
+template <typename T> T max(const T& a,const T& b)
 {
     return (a > b) ? a : b; 
 }
