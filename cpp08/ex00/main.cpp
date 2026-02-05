dfc2f637d999a33fef4da4e1870cf2edc494e810
userhm <dealkalr@gmail.com>
2026-02-05 14:25:59 +0100

    ok

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 11:11:15 by abhimi            #+#    #+#             */
-/*   Updated: 2026/01/04 11:56:02 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/05 14:24:11 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main()
     std::vector<int> a(2,6);
     try
     {
-        std::cout << easyfind(a,8) <<std::endl;
+        std::cout << easyfind(a,6) <<std::endl;
     }
     catch(const std::exception& e)
     {
