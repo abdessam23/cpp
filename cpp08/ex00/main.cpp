6fcdd13b43e0ea4fa824ef7f373ec3e1097a175b
userhm <dealkalr@gmail.com>
2026-02-09 14:10:40 +0100

    test

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 11:11:15 by abhimi            #+#    #+#             */
-/*   Updated: 2026/02/09 14:07:57 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/09 14:10:05 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 #include <vector>
 int main()
 {
-    std::vector<int> a(2,6);
+    std::vector<int> a= {1,3,4,5,6,7,8};
     try
     {
-        std::vector<int>::iterator it = easyfind(a,3);
+        std::vector<int>::iterator it = easyfind(a,7);
         std::cout << std::distance(a.begin(),it) <<std::endl;
     }
     catch(const std::exception& e)
