ac32cb5830ed7c5f42569d4a926e4b52ef4cf74a
userhm <dealkalr@gmail.com>
2026-02-09 15:13:39 +0100

    ok

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 10:43:06 by abhimi            #+#    #+#             */
-/*   Updated: 2026/02/09 14:05:22 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/09 15:11:14 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typename T::iterator    easyfind(T& a,int value)
 {
     typename T::iterator it = std::find(a.begin(),a.end(), value);
     if (it == a.end())
-        throw std::out_of_range("Not  found");
+        throw std::runtime_error("Not  found");
     return it;
 }
 
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,25 +6,46 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 11:11:15 by abhimi            #+#    #+#             */
-/*   Updated: 2026/02/09 14:10:05 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/09 15:13:13 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <vector>
+#include <deque>
+#include <list>
 int main()
 {
-    std::vector<int> a= {1,3,4,5,6,7,8};
+    std::vector<int> a;
+    a.push_back(0);
+    a.push_back(8);
+    a.push_back(5);
+    a.push_back(25);
+    a.push_back(3);
+    std::list<int> v;
+    v.push_back(1);
+    v.push_back(88);
+    v.push_back(3);
+    v.push_back(14);
+    v.push_back(3);
+    std::deque<int> c;
+    c.push_back(6);
+    c.push_back(3);
+    c.push_back(44);
+    c.push_back(2);
+    c.push_back(3);
     try
     {
-        std::vector<int>::iterator it = easyfind(a,7);
+        std::vector<int>::iterator it = easyfind(a,25);
         std::cout << std::distance(a.begin(),it) <<std::endl;
+        std::list<int>::iterator itl = easyfind(v,3); 
+        std::cout << std::distance(v.begin(),itl) <<std::endl;
+        std::deque<int>::iterator itd = easyfind(c,200);  
+        std::cout << std::distance(c.begin(),itd) <<std::endl;
     }
     catch(const std::exception& e)
     {
         
         std::cerr << e.what() << '\n';
     }
-    
-    
 }
