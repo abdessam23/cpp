f90493b6a923e60ffd00a80db800ed2db37851a7
userhm <dealkalr@gmail.com>
2026-02-09 13:57:11 +0100

    fix some bug in ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 10:43:06 by abhimi            #+#    #+#             */
-/*   Updated: 2026/02/05 14:47:44 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/09 13:56:51 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@
 #include <exception>
 
 template<typename T> 
-int    easyfind(T& a,int value)
+typename T::iterator    easyfind(T& a,int value)
 {
-    for(size_t i = 0;i < a.size();i++)
-    {
-        if (a[i] == value)
-            return i;
-    }
-    throw std::out_of_range("Not  Ffound");
+    typename T::iterator it = std::find(a.begin(),a.end(), value);
+    if (it == a.end())
+        throw std::out_of_range("Not  found");
+    return it;
 }
 
 #endif 
