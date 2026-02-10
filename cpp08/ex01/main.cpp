c757126bf774bf9a9e68bd0f00d1a50f9e868882
userhm <dealkalr@gmail.com>
2026-02-10 18:55:13 +0100

    ok

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 11:19:12 by abhimi            #+#    #+#             */
-/*   Updated: 2026/02/10 18:43:57 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/02/10 18:54:33 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 
 int main()
 {
-    Span sp = Span(20);
-    int arr[] = {2,1,3,4,5,6,77,8,86,1};
+    Span sp = Span(12);
+    
+    int arr[] = {2,14,3,4,5,6,77,8,86,1};
     try{
+        sp.addNumber(0);
         sp.addrange(arr,arr + 10);
         std::cout << sp.shortestSpan() << std::endl;
         std::cout << sp.longestSpan() << std::endl;
