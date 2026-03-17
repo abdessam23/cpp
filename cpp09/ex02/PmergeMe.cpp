ace568621e63dc62eb496331a1062e040fa36b0f
userhm <dealkalr@gmail.com>
2026-03-17 12:08:35 +0000

    ok

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -352,7 +352,7 @@ void PmergeMe::merge_insert(std::vector<int>& d)
     if (has_straggler)
         straggler = b[b.size() - 1];
 
-    std::cout <<"\n**** before rcursion a : " ;
+    std::cout <<"\n**** before rEcursion a : " ;
     for(size_t i = 0; i < a.size();i++)
     {
         std::cout << a[i] << " ";
