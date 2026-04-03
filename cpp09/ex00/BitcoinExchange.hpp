b91b3ebe93f5263458ea32616e82642943a56d22
userhm <dealkalr@gmail.com>
2026-04-03 16:53:59 +0100

    ok

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: abhimi <abhimi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 14:32:54 by abhimi            #+#    #+#             */
-/*   Updated: 2026/03/31 19:14:11 by abhimi           ###   ########.fr       */
+/*   Updated: 2026/04/01 00:04:31 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ class BitcoinExchange
         void check_value(float value) const;    
         void check_date(int y,int m,int d) const;  
         void find_result(std::string& date,float value) const; 
-        void find_result(const std::string& date, float value) const; 
+        void find_result(const std::string& date, float value) const;
+        
         BitcoinExchange(); 
         BitcoinExchange(const BitcoinExchange&);
         BitcoinExchange& operator=(const BitcoinExchange&); 
